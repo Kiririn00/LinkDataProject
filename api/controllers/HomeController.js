10971c6589b2d98e2b1ed0511821097ce68ea3ee
Varit <as.varit_st@tni.ac.th>
2015-12-15 00:11:36 +0900

    delete MySQL INSERT program & complete INSERT to FUSEKI !!

--- api/controllers/HomeController.js
+++ api/controllers/HomeController.js
@@ -1,13 +1,13 @@
 //this is HomeController
 
 var request = require('request');
+var http = require('http');
 
 
 module.exports = {
 
   index: function (req, res) {
     //return res.send("Hi there! 2");
-
 
     return res.view();
 
@@ -16,25 +16,47 @@ module.exports = {
   //this is function for test form by using sails
   Form: function (req, res) {
 
-    if(req.method=='POST') {
-      //get post data
-      var username = req.param('username');
-      var password = req.param('password');
-      var email = req.param('email');
-
-      //INSERT new data to mySQL
-      User.create({
-        username: username,
-        password: password,
-        email: email
-      }).exec(function CreateData(err,created){
-        console.log('create data:'+ created.username);
+    //set name of dataset that will INSERT
+    var dataset = 'users';
+    //set SPARQL that will query by Fuseki
+    var postData = 'PREFIX user: <http://testproject.com/user#>\n' +
+      'PREFIX data: <http://testproject.com/data#>\n' +
+      'INSERT DATA{\n' +
+      'user:test data:test "test"\n' +
+      '}';
+    //what wii send and what will set in header and body of HTTP POST REQUEST
+    var options = {
+      hostname: 'localhost',
+      port: 3030,
+      path: '/users/update',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/sparql-update',
+        'Content-Length': postData.length
+      }
+    };
+    //send http request to Fuseki
+    var req = http.request(options, function(res) {
+      console.log('STATUS: ' + res.statusCode);
+      console.log('HEADERS: ' + JSON.stringify(res.headers));
+      res.setEncoding('utf8');
+      res.on('data', function (chunk) {
+        console.log('BODY: ' + chunk);
       });
+      res.on('end', function() {
+        console.log('No more data in response.')
+      })
+    });
+    //if error for send HTTP request
+    req.on('error', function(e) {
+      console.log('problem with request: ' + e.message);
+    });
 
-      //debug what post data coming to server
-      console.log(username, email, password);
+    // write data to request body
+    req.write(postData);
+    req.end();
 
-    }//end if post
+    //show view
     return res.view();
   },
 
