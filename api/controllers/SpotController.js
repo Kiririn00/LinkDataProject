5f96fe79da2b7b611c4b06d5aa126fe669b7c0b1
Varit <as.varit_st@tni.ac.th>
2016-03-18 19:22:54 +0900

    create decision tree algorithm

--- api/controllers/SpotController.js
+++ api/controllers/SpotController.js
@@ -31,18 +31,110 @@ module.exports = {
       var category = req.param("category");
       var good_for = req.param("good_for");
 
+      var ati1 = 0,ati2 = 0,ati3 = 0;
+      var ans_case = 0 ;
+
       //debug post data
       console.log("POST DATA:"+type+" "+category+" "+good_for);
 
-      //decision tree algorithm
+      //--------------------decision tree algorithm-------------------------------------------//
 
-      //node1(attribute1)
-      if(type==""){
+      if(type == 0){//node layer 1 case 1
+        ati1 = 0;
+        if(category == 0){//node layer 2 case 1
+          ati2 = 0;
+          if(good_for == 0){//node layer 3 case 1
+            ati3 = 0;
+            ans_case = 1; //case 0,0,0
+          }
+          else if(good_for == 1){//node layer 3 case 2
+            ati3 = 1;
+            ans_case = 2;//case 0,0,1
+          }
+          else if(good_for == 2){//node layer 3 case 3
+            ati3 = 2;
+            ans_case = 3;//case 0,0,2
+          }
+          else{//error in layer 3
 
+          }
+        }
+        else if(category == 1){//node layer 2 case 2
+          ati2 = 1;
+          if(good_for == 0){//node layer 3 case 1
+            ati3 = 0;
+            ans_case = 4;//case 0,1,0
+          }
+          else if(good_for == 1){//node layer 3 case 2
+            ati3 = 1;
+            ans_case = 5;//case 0,1,1
+          }
+          else if(good_for == 2){//node layer 3 case 3
+            ati3 = 2;
+            ans_case = 6;//case 0,1,2
+          }
+          else{//error in layer 3
+
+          }
+        }
+        else{//error in layer 2
+
+        }
       }
 
-    }
+      else if(type == 1){// node layer 1 case 2
+        ati1 = 1;
+        if(category == 0){//node layer 2 case 1
+          ati2 = 0;
+          if(good_for == 0){//node layer 3 case 1
+            ati3 = 0;
+            ans_case = 7;//case 1,0,0
+          }
+          else if(good_for == 1){//node layer 3 case 2
+            ati3 = 1;
+            ans_case = 8;//case 1,0,1
+          }
+          else if(good_for == 2){//node layer 3 case 3
+            ati3 = 2;
+            ans_case = 9;//case 1,0,2
+          }
+          else{//error in layer 3
 
+          }
+        }
+        else if(category == 1){//node layer 2 case 2
+          ati2 = 1;
+          if(good_for == 0){//node layer 3 case 1
+            ati3 = 0;
+            ans_case = 10;//case 1,1,0
+          }
+          else if(good_for == 1){//node layer 3 case 2
+            ati3 = 1;
+            ans_case = 11;//case 1,1,1
+          }
+          else if(good_for == 2){//node layer 3 case 3
+            ati3 = 2;
+            ans_case = 12;//case 1,1,2
+          }
+          else{//error in layer 3
+
+          }
+        }
+        else{//error in layer 2
+
+        }
+
+      }
+      else{//error in layer 1
+
+      }
+      console.log("attribute: "+ati1+" "+ati2+" "+ati3);//debug attributes
+      console.log("answer case:"+ans_case);
+      //--------------- end decision tree algorithm---------------------------------------//
+
+
+
+    }//end if post
 
     return res.view();
   },
